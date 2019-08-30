75a34f46899995786bc40604dbb34f4276f49cff
vk <vk>
2019-08-29 21:33:58 -0500

    Add comments

--- Lab1_Part2.py
+++ Lab1_Part2.py
@@ -1,5 +1,10 @@
+#list of classes
 list_of_classes = []
+
+#asking the classes that the user take this semester
 number_of_class = input("How many classes are you taking this semester? ")
+
+#loop of asking the user to enter the name of the class
 while True:
     class_name = ('Enter the name of the class: ')
     list_of_classes.append(class_name)
