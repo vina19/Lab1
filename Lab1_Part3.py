c7228a0502b1e29ebd3459108c85723585f585f7
vina19 <sj9962nf@go.minneapolis.edu>
2019-09-02 21:18:54 -0500

    Modify Part 3

--- Lab1_Part3.py
+++ Lab1_Part3.py
@@ -3,19 +3,18 @@ import random
 #the random number that the computer think
 random_number = random.randint(1, 10)
 
-#ask the user to guess
-print('Please take a guess in range 1-10')
-
-#user input number
-user_guess = int(input())
-
 #message to the user if the number is too low or too high
-while user_guess != random_number:
+while True:
+    #ask the user to guess
+    print('Please take a guess in range 1-10')
+
+    #user input number
+    user_guess = int(input())
+
     if user_guess < random_number:
         print('It is too low')
-        break
     elif user_guess > random_number:
         print('It is too high')
-        break
-    else:
+    elif user_guess == random_number:
+        print('You got it!')
         break
