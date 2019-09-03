06d45cf8f341a1f5be8be493602f6fee88ecd4f6
vina19 <sj9962nf@go.minneapolis.edu>
2019-09-02 22:13:28 -0500

    Modify Part 2 and 4

--- Lab1_Part2.py
+++ Lab1_Part2.py
@@ -2,15 +2,15 @@
 list_of_classes = []
 
 #asking the classes that the user take this semester
-number_of_class = input("How many classes are you taking this semester? ")
+number_of_class = int(input('How many classes are you taking this semester? '))
 
 #loop of asking the user to enter the name of the class
-while True:
-    class_name = ('Enter the name of the class: ')
+for n in range(number_of_class):
+    class_name = input('Enter the name of the class: ')
     list_of_classes.append(class_name)
     
-print("The classes you are taking are: ")
+print('The classes you are taking are: ')
 
 for name in list_of_classes:
-    print(list_of_classes + '\n')
+    print('\n'.join(list_of_classes))
 
